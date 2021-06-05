0f657190d436ff653426395d43e2bacde1c40989
grantmwilson <81658019+grantmwilson@users.noreply.github.com>
2021-06-05 16:33:29 -0400

    Added Message class to common

---
+++ engfrosh_common/DiscordMessage.py
@@ -0,0 +1,10 @@
+class Message:
+
+    def __init__(self, text="", identifier="", file_path=None, display_filename="") -> None:
+        self.identifier = identifier
+        self.text = text
+        self.file_path = file_path
+        self.display_filename = display_filename
+
+    
+
