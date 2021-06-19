761eb85d7e2d701f12bccf0c881cd92c1a110a86
Grant Wilson <81658019+grantmwilson@users.noreply.github.com>
2021-06-19 15:12:37 -0400

    Added database models for storing scav data. (#47)
    * Added question / hint support to database
    
    * Added Message class to common
    
    * Added question / hint support to database
    
    * Added Message class to common
    
    * Removed question order model
    
    * Added scavenger eam support to the interface.
    
    * Fixed formatting

--- engfrosh_common/init_test_db.py
+++ engfrosh_common/init_test_db.py
@@ -1,8 +1,33 @@
 import asyncio
+import logging
+import os
 
 from DatabaseInterface import DatabaseInterface
 
 DATABASE_PATH = ""
+
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+LOG_LEVEL = logging.DEBUG
+
+# region Logging Setup
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+
+logger = logging.getLogger(SCRIPT_NAME)
+
+if __name__ == "__main__":
+    LOG_FILE = CURRENT_DIRECTORY + "/{}.log".format(SCRIPT_NAME)
+    if os.path.exists(LOG_FILE):
+        try:
+            os.remove(LOG_FILE)
+        except PermissionError:
+            pass
+    logging.basicConfig(level=LOG_LEVEL, filename=LOG_FILE)
+    logger.debug("Module started.")
+    logger.debug("Log file set as: %s", LOG_FILE)
+
+logger.debug("Set current directory as: %s", CURRENT_DIRECTORY)
+# endregion
 
 
 async def main():
@@ -19,7 +44,8 @@ async def main():
     await interface.add_scav_channel(scav_channel_id, group_id)
     await interface.add_discord_user(user_discord_id, user_id)
     await interface.add_user_to_group(user_id, group_id=group_id)
-    await interface.add_team(group_id, "Best Team")
+    await interface.add_frosh_team(group_id, "Best Team")
+    await interface.add_scavenger_team(group_id)
 
     # ** Example Usage **
     # **  Comment Out  **
@@ -36,6 +62,7 @@ async def example_checks(interface: DatabaseInterface):
     discord_user_id = 111111111111111111
     user_id = 16
     coin_change = 37
+    locked_out_until = "2022-02-02 17:47:32-04:00"
 
     print("\nExample of Checks")
     print("====================================================")
@@ -64,6 +91,14 @@ async def example_checks(interface: DatabaseInterface):
     await interface.update_coin_amount(coin_change, group_id=group_id)
     res = await interface.get_coin_amount(group_id=group_id)
     print(f"Added {coin_change} to team with id [{group_id}] totalling {res}.")
+
+    await interface.set_team_locked_out_time(group_id, 10)
+    res = await interface.get_team_locked_out_time(group_id)
+    print(f"Team with id [{group_id}] locked out of scav until {res}")
+
+    await interface.set_team_locked_out_time(group_id, end_time=locked_out_until)
+    res = await interface.get_team_locked_out_time(group_id)
+    print(f"Team with id [{group_id}] locked out of scav until {res}")
 
     print("====================================================\n")
 
