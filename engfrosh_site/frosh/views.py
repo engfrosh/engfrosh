59524d345fbb9790a7b9546daecf4f270b04c21f
grantmwilson <development@grantmwilson.com>
2021-06-26 16:35:13 -0400

    Working web leaderboard for scoin.

--- engfrosh_site/frosh/views.py
+++ engfrosh_site/frosh/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render # noqa F401
+from django.shortcuts import render  # noqa F401
+from django.http import HttpRequest
+
+from .models import Team
 
 # Create your views here.
+
+
+def coin_standings(request: HttpRequest):
+    context = {
+        "teams": []
+    }
+
+    teams = Team.objects.all()
+
+    for team in teams:
+        d = {}
+
+        d["name"] = team.display_name
+        d["coin"] = team.coin_amount
+        d["place"] = 0
+        context["teams"].append(d)
+
+    return render(request, "scoin_standings.html", context)
