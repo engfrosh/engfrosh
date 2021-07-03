7f9758464fafc243a81dd138d55a64017eb38f09
pruthvichivukula <41833442+pruthvichivukula@users.noreply.github.com>
2021-07-03 19:38:35 -0400

    added movable code at bottom for using the function to another file

--- engfrosh_common/AWS_SES.py
+++ engfrosh_common/AWS_SES.py
@@ -1,40 +1,10 @@
 import boto3
 from botocore.exceptions import ClientError
 
-# Replace sender@example.com with your "From" address.
-# This address must be verified with Amazon SES.
-SENDER = "pruthvi.chivukula@gmail.com"
-
-# Replace recipient@example.com with a "To" address. If your account
-# is still in the sandbox, this address must be verified.
-RECIPIENT = "pruthvi.chivukula@gmail.com"
-
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the
 # ConfigurationSetName=CONFIGURATION_SET argument below.
 # CONFIGURATION_SET = "ConfigSet"
-
-# The subject line for the email.
-# SUBJECT = "Amazon SES Test (SDK for Python)"
-
-# The email body for recipients with non-HTML email clients.
-# BODY_TEXT = ("Amazon SES Test (Python)\r\n"
-#              "This email was sent with Amazon SES using the "
-#              "AWS SDK for Python (Boto)."
-#              )
-
-# The HTML body of the email.
-# BODY_HTML = """<html>
-# <head></head>
-# <body>
-#   <h1>Amazon SES Test (SDK for Python)</h1>
-#   <p>This email was sent with
-#     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-#     <a href='https://aws.amazon.com/sdk-for-python/'>
-#       AWS SDK for Python (Boto)</a>.</p>
-# </body>
-# </html>
-#             """
 
 def send_SES(sender_email, recipient_email, subject, body_text, body_html):
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
@@ -72,10 +42,10 @@ def send_SES(sender_email, recipient_email, subject, body_text, body_html):
                 },
             },
             Source=sender_email
-            #,
+            # ,
             # If you are not using a configuration set, comment or delete the
             # following line
-            #ConfigurationSetName=CONFIGURATION_SET,
+            # ConfigurationSetName=CONFIGURATION_SET,
         )
     # Display an error if something goes wrong.
     except ClientError as e:
@@ -85,7 +55,42 @@ def send_SES(sender_email, recipient_email, subject, body_text, body_html):
         print(response['MessageId'])
 
 
-#if __name__ == '__main__':
-#    send_SES(SENDER, RECIPIENT, "AWS SES test email with custom subject", BODY_TEXT)
+# The following section can be added to another python file (once this file is imported) to added sending
+# functionality to that file.
 
+# Uncomment the following line to import the file
+# import engfrosh_common.AWS_SES as SES
 
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SES_Sender = "pruthvi.chivukula@gmail.com"
+
+# Replace recipient@example.com with a "To" address. If your account
+# is still in the sandbox, this address must be verified.
+SES_Recipient = "pruthvi.chivukula@gmail.com"
+
+# The subject line for the email.
+SES_Subject = "This is a test subject part 2"
+
+# The email body for recipients with non-HTML email clients.
+SES_BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto). https://aws.amazon.com/ses/"
+                 )
+
+# The HTML body of the email. Change the body as you like
+SES_BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (SDK for Python)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+      AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+            """
+
+if __name__ == '__main__':
+    send_SES(SES_Sender, SES_Recipient, SES_Subject, SES_BODY_TEXT, SES_BODY_TEXT)
+    #SES.send_SES(SES_Sender, SES_Recipient, SES_Subject, SES_BODY_TEXT, SES_BODY_HTML)
