b431d017fcca2dc3e743b0ec726dee16d4984385
grantmwilson <development@grantmwilson.com>
2021-07-05 18:11:12 -0400

    Adjusted requirements, added logging, rearranged files.

--- engfrosh_common/AWS_SES.md
+++ engfrosh_common/AWS_SES.md
@@ -1,18 +1,18 @@
 # AWS SES
 
-##Notes
+## Notes
 ### AWS Account Limitations
 - By default, an AWS may be in the "Amazon SES sandbox." This limits to whom you can send an email (verified emails only).
-- More info about this, as well as how to move out of the sandbox can be found here: 
-  https://docs.aws.amazon.com/ses/latest/DeveloperGuide/request-production-access.html
+- More info about this, as well as how to move out of the sandbox can be found 
+[here](https://docs.aws.amazon.com/ses/latest/DeveloperGuide/request-production-access.html)
 
 ### Credentials Note 
 - In order to use boto3 (AWS's python library) locally, you must add a credentials file for authentication.
-- Instructions on how to create this file can be found here: 
-  https://docs.aws.amazon.com/ses/latest/DeveloperGuide/create-shared-credentials-file.html
+- Instructions on how to create this file can be found
+[here](https://docs.aws.amazon.com/ses/latest/DeveloperGuide/create-shared-credentials-file.html)
   - This file MAY be able to be moved, depending on the situation, however I have not looked into it as it seems 
-    unnecessary. However, this link may help should you wish to do so:
-    https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+    unnecessary. However, [this link](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html)
+    may help should you wish to do so:
 - As always with these types of files, make sure you do not add it to your GIT/VCS, or you may have a bad time when 
   someone starts using your account without permission.
   
--- engfrosh_common/AWS_SES.py
+++ engfrosh_common/AWS_SES.py
@@ -1,10 +1,36 @@
 import boto3
+import logging
+import os
 from botocore.exceptions import ClientError
 
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the
 # ConfigurationSetName=CONFIGURATION_SET argument below.
 # CONFIGURATION_SET = "ConfigSet"
+
+
+CURRENT_DIRECTORY = os.path.dirname(__file__)
+LOG_LEVEL = logging.DEBUG
+
+# region Logging Setup
+SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
+
+logger = logging.getLogger(SCRIPT_NAME)
+
+if __name__ == "__main__":
+    LOG_FILE = CURRENT_DIRECTORY + "/{}.log".format(SCRIPT_NAME)
+    if os.path.exists(LOG_FILE):
+        try:
+            os.remove(LOG_FILE)
+        except PermissionError:
+            pass
+    logging.basicConfig(level=LOG_LEVEL, filename=LOG_FILE)
+    logger.debug("Module started.")
+    logger.debug("Log file set as: %s", LOG_FILE)
+
+logger.debug("Set current directory as: %s", CURRENT_DIRECTORY)
+# endregion
+
 
 def send_SES(sender_email, recipient_email, subject, body_text, body_html):
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
@@ -49,10 +75,10 @@ def send_SES(sender_email, recipient_email, subject, body_text, body_html):
         )
     # Display an error if something goes wrong.
     except ClientError as e:
-        print(e.response['Error']['Message'])
+        logger.error(e.response['Error']['Message'])
+        return False
     else:
-        print("Email sent! Message ID:"),
-        print(response['MessageId'])
+        logger.info(f"Email sent! Message ID: {response['MessageId']}"),
 
 
 # The following section can be added to another python file (once this file is imported) to added sending
@@ -63,11 +89,11 @@ def send_SES(sender_email, recipient_email, subject, body_text, body_html):
 
 # Replace sender@example.com with your "From" address.
 # This address must be verified with Amazon SES.
-SES_Sender = "pruthvi.chivukula@gmail.com"
+SES_Sender = "noreply@engfrosh.com"
 
 # Replace recipient@example.com with a "To" address. If your account
 # is still in the sandbox, this address must be verified.
-SES_Recipient = "pruthvi.chivukula@gmail.com"
+SES_Recipient = "technical@engfrosh.com"
 
 # The subject line for the email.
 SES_Subject = "This is a test subject part 2"
