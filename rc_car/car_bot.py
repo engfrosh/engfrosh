be0caab98f887a99eb020bb8dfca696854bf9b6d
Riley Rogers <rileyrogers30@gmail.com>
2021-08-25 12:50:39 -0400

    added more discord commands and duration

--- rc_car/car_bot.py
+++ rc_car/car_bot.py
@@ -13,24 +13,64 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if "w" == message.content.lower():
-        print("FORWARD")
-        await message.channel.send("Moving Car Forward")
-        benny.drive(60)
+    lst = message.content.split()
 
-    if "a" == message.content.lower():
-        print("LEFT")
-        await message.channel.send("Moving Car Left")
-        benny.turn_left(80)
+    if lst.__len__() == 2:
+        pass
 
-    if "s" == message.content.lower():
-        print("REVERSE")
-        await message.channel.send("Moving Car Backwards")
-        benny.back(60)
+    if lst.__len__() == 1 or lst.__len__() == 3:
+        # forward
+        if "w" == message.content.lower():
+            await message.channel.send("Moving Car Forward")
+            benny.drive(60, 0.5)
+        elif "w" == lst[0].lower():
+            if lst[1].isdigit() and isfloat(lst[2]):
+                await message.channel.send("Moving Car Forward")
+                benny.drive(int(lst[1]), float(lst[2]))
+            else:
+                pass
 
-    if "d" == message.content.lower():
-        print("RIGHT")
-        await message.channel.send("Moving Car Right")
-        benny.turn_right(80)
+        # left
+        if "a" == message.content.lower():
+            await message.channel.send("Moving Car Left")
+            benny.turn_left(80, 0.5)
+        elif "a" == lst[0].lower():
+            if lst[1].isdigit() and isfloat(lst[2]):
+                await message.channel.send("Moving Car Left")
+                benny.turn_left(int(lst[1]), float(lst[2]))
+            else:
+                pass
+
+        # reverse
+        if "s" == message.content.lower():
+            await message.channel.send("Moving Car Backwards")
+            benny.back(60, 0.5)
+        elif "s" == lst[0].lower():
+            if lst[1].isdigit() and isfloat(lst[2]):
+                await message.channel.send("Moving Car Left")
+                benny.back(int(lst[1]), float(lst[2]))
+            else:
+                pass
+
+        # right
+        if "d" == message.content.lower():
+            await message.channel.send("Moving Car Right")
+            benny.turn_right(80, 0.5)
+        elif "d" == lst[0].lower():
+            if lst[1].isdigit() and isfloat(lst[2]):
+                await message.channel.send("Moving Car Left")
+                benny.turn_right(int(lst[1]), float(lst[2]))
+            else:
+                pass
+
+
+def isfloat(msg):
+    try:
+        float(msg)
+        return True
+    except:
+        print("not float")
+        return False
+
 
 client.run("ODc2OTYxODMzOTE4MDA1MjU4.YRrsWg.ie4fqSkSqrZ0gjhvX0dFMRJJ9PY")
--- rc_car/rc_car_controller.py
+++ rc_car/rc_car_controller.py
@@ -1,74 +1,74 @@
 import PiMotor
 import time
 
-
 class RC_Car:
 
-    def __init__(self, speed):
+    def __init__(self,speed):
         self.speed = speed
-        self._back_left = PiMotor.Motor("MOTOR1", 2)
-        self._back_right = PiMotor.Motor("MOTOR2", 1)
-        self._front_left = PiMotor.Motor("MOTOR4", 1)
-        self._front_right = PiMotor.Motor("MOTOR3", 2)
-        self._all_motors = PiMotor.LinkedMotors(
-            self._front_left, self._front_right, self._back_left, self._back_right)
-        self._left_motors = PiMotor.LinkedMotors(
-            self._front_left, self._back_left)
-        self._right_motors = PiMotor.LinkedMotors(
-            self._front_right, self._back_right)
+        self._back_left = PiMotor.Motor("MOTOR1",2)
+        self._back_right = PiMotor.Motor("MOTOR2",1)
+        self._front_left = PiMotor.Motor("MOTOR4",1)
+        self._front_right = PiMotor.Motor("MOTOR3",2)
+        self._all_motors = PiMotor.LinkedMotors(self._front_left, self._front_right, self._back_left, self._back_right)
+        self._left_motors = PiMotor.LinkedMotors(self._front_left, self._back_left)
+        self._right_motors = PiMotor.LinkedMotors(self._front_right, self._back_right)
 
-        self._arrow_forward = PiMotor.Arrow(3)
+        self._arrow_forward = PiMotor.Arrow(3) 
         self._arrow_back = PiMotor.Arrow(1)
         self._arrow_left = PiMotor.Arrow(2)
         self._arrow_right = PiMotor.Arrow(4)
 
-    # def handle_command(self, speed:float, direction:str, time:float):
+    #def handle_command(self, speed:float, direction:str, time:float):
     #    pass
 
     # test function to make sure all motors work
-    def test_motor(self, speed):
+    def test_motor(self, speed, duration):
         self._front_left.forward(speed)
-        time.sleep(1)
+        time.sleep(duration)
         self._front_left.stop()
 
         self._front_right.forward(speed)
-        time.sleep(1)
+        time.sleep(duration)
         self._front_right.stop()
 
         self._back_left.forward(speed)
-        time.sleep(1)
+        time.sleep(duration)
         self._back_left.stop()
 
         self._back_right.forward(speed)
-        time.sleep(1)
+        time.sleep(duration)
         self._back_right.stop()
 
-    def back(self, speed):
+    def back(self, speed, duration):
         self._all_motors.reverse(self.speed)
         self._arrow_back.on()
-        time.sleep(0.75)
-        self._all_motors.stop()
+        time.sleep(duration)
+        self._all_motors.stop() 
         self._arrow_back.off()
 
-    def drive(self, speed):
+    def drive(self, speed, duration):
         self._all_motors.forward(self.speed)
         self._arrow_forward.on()
-        time.sleep(0.75)
-        self._all_motors.stop()
+        time.sleep(duration)
+        self._all_motors.stop() 
         self._arrow_forward.off()
 
-    def turn_left(self, speed):
+    def turn_left(self, speed, duration):
         self._right_motors.forward(self.speed)
-        time.sleep(1)
+        self._arrow_left.on()
+        time.sleep(duration)
         self._all_motors.stop()
+        self._arrow_left.off()
 
-    def turn_right(self, speed):
+    def turn_right(self, speed, duration):
         self._left_motors.forward(self.speed)
-        time.sleep(1)
+        self._arrow_right.on()
+        time.sleep(duration)
         self._all_motors.stop()
+        self._arrow_right.off()
 
 #benny = RC_Car(60)
 
-# benny.back(50)
-# benny.turn_left(70)
-# benny.test_motor(60)
+#benny.back(50)
+#benny.turn_left(70)
+#benny.test_motor(60)
