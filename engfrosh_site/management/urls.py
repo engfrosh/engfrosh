4c5500a1417c42769cabd070022814abea7bea9c
grantmwilson <development@grantmwilson.com>
2021-09-01 10:48:48 -0400

    Minor fixes ignoring role management

--- engfrosh_site/management/urls.py
+++ engfrosh_site/management/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('teams', views.manage_frosh_teams, name="manage_frosh_teams"),
     path('users/add-to-guild', views.add_discord_user_to_guild, name="add_users_to_discord_guild"),
     path('', views.manage_index, name="manage_index"),
-    path('roles', views.manage_roles, name="manage_roles"),
+    # path('roles', views.manage_roles, name="manage_roles"),
     path('discord/channels', views.manage_discord_channels, name="manage_discord_channels"),
     path('discord/channel-groups', views.manage_discord_channel_groups, name="manage_discord_channel_groups")
 ]
