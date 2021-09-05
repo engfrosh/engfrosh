dbab9ded859e92d61b448eb0dc6f9c0446c11c3a
grantmwilson <development@grantmwilson.com>
2021-09-05 13:58:57 -0400

    Hot patch for discord user admin

--- engfrosh_site/authentication/admin.py
+++ engfrosh_site/authentication/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import DiscordUser, MagicLink
 
-admin.site.register([DiscordUser, MagicLink])
+admin.site.register([MagicLink])
+
+
+class DiscordUserAdmin(admin.ModelAdmin):
+    """Admin for Discord Users."""
+
+    list_display = ('discord_username', 'user')
+
+
+admin.site.register(DiscordUser, DiscordUserAdmin)
