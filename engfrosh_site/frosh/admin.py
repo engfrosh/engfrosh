6cd61bcb314e7a67f0606e47a7830e494a15b3de
grantmwilson <development@grantmwilson.com>
2021-09-05 14:18:35 -0400

    Add search to fields to user details and discord users

--- engfrosh_site/frosh/admin.py
+++ engfrosh_site/frosh/admin.py
@@ -4,4 +4,13 @@ from django.contrib import admin
 
 from .models import Team, FroshRole, UserDetails, UniversityProgram
 
-admin.site.register([Team, FroshRole, UserDetails, UniversityProgram])
+admin.site.register([Team, FroshRole, UniversityProgram])
+
+
+class UserDetailsAdmin(admin.ModelAdmin):
+    """User Details Admin."""
+
+    search_fields = ('user', 'name')
+
+
+admin.site.register(UserDetails, UserDetailsAdmin)
