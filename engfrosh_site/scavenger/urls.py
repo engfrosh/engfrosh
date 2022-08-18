d339e0e2793b68603da060447a55de5057019db9
grantmwilson <development@grantmwilson.com>
2022-08-18 13:15:50 -0400

    Added puzzle verification upload page

--- engfrosh_site/scavenger/urls.py
+++ engfrosh_site/scavenger/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name="scavenger_index"),
     path('puzzle/', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
     path('puzzle//', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
-    path('puzzle/<slug:slug>/', views.puzzle_view)
+    path('puzzle/<slug:slug>/', views.puzzle_view),
+    path("puzzle/<slug:slug>/verification_photo/", views.puzzle_photo_verification_view)
 ]
--- engfrosh_site/scavenger/views.py
+++ engfrosh_site/scavenger/views.py
@@ -1,9 +1,9 @@
 from typing import Optional, Union
 from django.http import HttpRequest, HttpResponse
-from django.http.response import HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
+from django.http.response import HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse, HttpResponseForbidden
 from django.shortcuts import render  # noqa F401
 
-from common_models.models import Puzzle, Team
+from common_models.models import Puzzle, Team, VerificationPhoto
 from django.contrib.auth.decorators import login_required, permission_required
 
 import logging
@@ -64,16 +64,63 @@ def puzzle_view(request: HttpRequest, slug: str) -> HttpResponse:
         if "answer" not in req_dict:
             return HttpResponseBadRequest("No answer provided in json body")
 
-        correct, stream_completed, next_puzzle = puz.check_team_guess(team, req_dict["answer"])
+        correct, stream_completed, next_puzzle, require_verification_photo = puz.check_team_guess(
+            team, req_dict["answer"])
 
-        if next_puzzle:
-            next_puzzle_id = next_puzzle.secret_id
+        if require_verification_photo:
+            next_page = "verification_photo/"
+        elif next_puzzle:
+            next_page = next_puzzle.secret_id
         else:
-            next_puzzle_id = ""
+            next_page = ""
 
         return JsonResponse({"correct": correct, "scavenger_stream_completed": stream_completed,
-                             "next_puzzle_id": next_puzzle_id, "require_verification_photo": False})
-        # TODO add verification photo submission
+                             "next": next_page})
 
     else:
         return HttpResponseNotAllowed(["GET", "POST"])
+
+
+@login_required(login_url='/accounts/login')
+def puzzle_photo_verification_view(request: HttpRequest, slug: str) -> HttpResponse:
+
+    team = Team.from_user(request.user)
+    if not team:
+        return HttpResponse("Sorry you aren't on a team. If this is incorrect, please contact supoort")
+
+    try:
+        puz: Union[Puzzle, None] = Puzzle.objects.get(secret_id=slug)
+    except Puzzle.DoesNotExist:
+        puz = None
+
+    if not (puz and puz.is_viewable_for_team(team)):
+        return HttpResponse("You do not have access to this puzzle.")
+
+    if not puz.requires_verification_photo_by_team(team):
+        return HttpResponse("You do not need to verify for this puzzle.")
+
+    match request.method:
+        case "GET":
+
+            context = {}
+
+            return render(request, "scavenger_verification_photo_upload.html", context)
+
+        case "POST":
+
+            pa = puz.puzzle_activity_from_team(team)
+            if not pa:
+                return HttpResponseForbidden()
+
+            photo = VerificationPhoto()
+            photo.photo = request.FILES["photo_upload"]
+            photo.save()
+
+            pa.verification_photo = photo
+            pa.save()
+
+            return HttpResponse()
+
+        case _:
+
+            return HttpResponseNotAllowed(["GET", "POST"])
