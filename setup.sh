44a2fbd3e0c16a9f09eaf6d95abdebde1b9342c5
Finlay <finman292004@protonmail.com>
2022-12-21 15:37:52 -0500

    Fixed timing of apt installs and used abs paths

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #! /usr/bin/bash
 
-cd ~/engfrosh
-git submodule init
-git submodule update
-pip install -r requirements.txt
-
-cd ~/
 
 apt update
 apt upgrade -y
 
 apt install -y postgresql postgresql-contrib nginx python3-pip gunicorn certbot python3-certbot-nginx
 
+cd /home/ubuntu/engfrosh
+git submodule init
+git submodule update
+pip install -r requirements.txt
