5b326e79730c276914af51a984e41d4f13c37bb3
finlaywashere <finman292004@protonmail.com>
2022-12-21 16:02:25 -0500

    Removed postgresql from setup.sh as it will be handled in a seperate container

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 apt update
 apt upgrade -y
 
-apt install -y postgresql postgresql-contrib nginx python3-pip gunicorn certbot python3-certbot-nginx
+apt install -y nginx python3-pip gunicorn certbot python3-certbot-nginx
 
 cd /home/ubuntu/engfrosh
 git submodule init
