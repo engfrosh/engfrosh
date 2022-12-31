fe2dca7667a2bc066925a7eedcde181c4d23ecb9
finlaywashere <finman292004@protonmail.com>
2022-12-31 16:32:17 -0500

    Updated frosh template to have get help button pointing to ticket system and made the ticket system a bit prettier

--- engfrosh_site/ticket/urls.py
+++ engfrosh_site/ticket/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.create_ticket),
-    path('create', views.create_ticket),
+    path('create', views.create_ticket, name="create_ticket"),
     path('view/<int:id>', views.view_ticket),
     path('view/<int:id>/comment', views.create_comment),
     path('view/<int:id>/action', views.ticket_action),
