6612e1316ac87bf39b99b4fcdb7abc3ec722ebe5
finlaywashere <finman292004@protonmail.com>
2023-02-13 22:00:22 -0500

    Fixed linter issue

--- engfrosh_site/calendar/views.py
+++ engfrosh_site/calendar/views.py
@@ -3,7 +3,6 @@ from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
 
 
-
 @login_required(login_url='/accounts/login')
 def index(request: HttpRequest):
     return render(request, "calendar_index.html", {})
