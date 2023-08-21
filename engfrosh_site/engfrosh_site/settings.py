87e37ab7150345335dceac379983a661d48f0dce
Finlay Maroney <finman292004@protonmail.com>
2023-08-21 00:16:31 -0400

    Fixed linter issues

--- engfrosh_site/engfrosh_site/settings.py
+++ engfrosh_site/engfrosh_site/settings.py
@@ -64,20 +64,20 @@ if not MICROSOFT_ID:
     logging.warning("No microsoft client id has been provided")
 
 sentry_sdk.init(
-dsn=os.environ.get("SENTRY_DSN"),
-integrations=[DjangoIntegration()],
-# If you wish to associate users to errors (assuming you are using
-# django.contrib.auth) you may enable sending PII data.
-send_default_pii=True,
-# Set traces_sample_rate to 1.0 to capture 100%
-# of transactions for performance monitoring.
-# We recommend adjusting this value in production.
-traces_sample_rate=1.0,
-# To set a uniform sample rate
-# Set profiles_sample_rate to 1.0 to profile 100%
-# of sampled transactions.
-# We recommend adjusting this value in production,
-profiles_sample_rate=1.0,
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # To set a uniform sample rate
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production,
+    profiles_sample_rate=1.0,
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
