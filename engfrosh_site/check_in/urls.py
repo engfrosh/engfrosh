24d974820f5e4c79c4c83b417586caa34c553db3
Finlay Maroney <finman292004@protonmail.com>
2023-08-24 23:00:30 -0400

    Added check in menu to main page

--- engfrosh_site/check_in/urls.py
+++ engfrosh_site/check_in/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path('check-in/<int:id>', views.check_in_view),
-    path('', views.check_in_index),
+    path('', views.check_in_index, name="check-in-index"),
     path('monitor', views.check_in_monitor)
 ]
