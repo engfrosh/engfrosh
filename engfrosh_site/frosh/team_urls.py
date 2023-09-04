61c80545d164e67bf0e9e205893f6a06cc8c5207
finlaywashere <finman292004@protonmail.com>
2023-09-04 11:58:38 -0400

    added link to skash

--- engfrosh_site/frosh/team_urls.py
+++ engfrosh_site/frosh/team_urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('coin/', views.coin_standings),
+    path('coin/', views.coin_standings, name="skash_standings"),
     path('mycoin', views.my_coin),
     path('my-skash', views.my_coin, name="my_skash")
 ]
