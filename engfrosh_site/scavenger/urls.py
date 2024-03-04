14ddcae4701dc4452c2d172f4b80269e9bb24584
finlaywashere <finman292004@protonmail.com>
2024-03-04 17:34:12 -0500

    Made puzzles use text format

--- engfrosh_site/scavenger/urls.py
+++ engfrosh_site/scavenger/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name="scavenger_index"),
     path('puzzle/', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
     path('puzzle//', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
-    path('puzzle/<slug:slug>/', views.puzzle_view),
+    path('puzzle/<slug:slug>/', views.puzzle_view, name="puzzle_view"),
     path("puzzle/<slug:slug>/verification_photo/", views.puzzle_photo_verification_view),
     path("stream_completed", views.stream_view),
     path("regen_trees", views.regen_trees, name="regen_trees")
