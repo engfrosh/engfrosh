39bbebf5ca2b7c2dd05b4ef47c855a26c34fd0be
finlaywashere <finman292004@protonmail.com>
2024-03-04 18:54:35 -0500

    Added some of the old puzzles view code

--- engfrosh_site/scavenger/urls.py
+++ engfrosh_site/scavenger/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name="scavenger_index"),
     path('puzzle/', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
     path('puzzle//', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
-    path('puzzle/<slug:slug>/', views.puzzle_view),
+    path('puzzle/<slug:slug>/', views.puzzle_view, name="view_puzzle"),
     path("puzzle/<slug:slug>/verification_photo/", views.puzzle_photo_verification_view),
     path("stream_completed", views.stream_view),
     path("regen_trees", views.regen_trees, name="regen_trees")
