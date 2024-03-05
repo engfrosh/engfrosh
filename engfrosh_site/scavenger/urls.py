96ce0084318ee2b6934db0f141fa7034a2806429
Finlay Maroney <finman292004@protonmail.com>
2024-03-04 19:08:16 -0500

    Got old view fully working

--- engfrosh_site/scavenger/urls.py
+++ engfrosh_site/scavenger/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('puzzle/', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
     path('puzzle//', RedirectView.as_view(pattern_name="scavenger_index", permanent=False)),
     path('puzzle/<slug:slug>/', views.puzzle_view, name="view_puzzle"),
-    path("puzzle/<slug:slug>/verification_photo/", views.puzzle_photo_verification_view),
+    path("puzzle/<slug:slug>/verification_photo/", views.puzzle_photo_verification_view, name="verify_puzzle"),
     path("stream_completed", views.stream_view),
     path("regen_trees", views.regen_trees, name="regen_trees")
 ]
