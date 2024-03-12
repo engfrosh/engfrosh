7e0cef093b1275c4373731cc05747eec4a2410c4
finlaywashere <finman292004@protonmail.com>
2024-03-11 21:01:12 -0400

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ apt install -y nginx python3-pip gunicorn certbot python3-certbot-nginx
 cd /home/ubuntu/engfrosh
 git submodule init
 git submodule update
-pip install -r requirements.txt
+pip3 install -r requirements.txt
