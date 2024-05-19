64611d642f5b4dd6dfdd7de5d911807cb8f9bba2
Finlay Maroney <finlay@finlaym.xyz>
2024-05-19 19:04:24 -0400

    Renamed inclusivity pages

--- engfrosh_site/frosh/user_urls.py
+++ engfrosh_site/frosh/user_urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.user_home, name="user_home"),
-    path('inclusivity_public', views.inclusivity_public, name="inclusivity_public"),
-    path('inclusivity_private', views.inclusivity_private, name="inclusivity_private"),
+    path('resources_public', views.inclusivity_public, name="inclusivity_public"),
+    path('resources_private', views.inclusivity_private, name="inclusivity_private"),
     path('view_event/<int:id>', views.view_event, name="view_event"),
     path('upload_charter', views.upload_charter, name="upload_charter"),
     path('faq/<int:id>', views.faq_page, name="view_faq")
