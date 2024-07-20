5cf2bf2630bd128fb6b90b3d2095122534e6711b
Finlay Maroney <finlay@finlaym.xyz>
2024-07-20 16:31:06 -0400

    fixed brackets around pings

--- engfrosh_site/scavenger/views.py
+++ engfrosh_site/scavenger/views.py
@@ -175,7 +175,7 @@ def puzzle_photo_verification_view(request: HttpRequest, slug: str) -> HttpRespo
             pa.save()
 
             DiscordChannel.send_to_updates_channels(
-                f"""@&1234213434590236763 {team.display_name} has uploaded a photo for {puz.name}""" +
+                f"""<@&1234213434590236763> {team.display_name} has uploaded a photo for {puz.name}""" +
                 """that needs verification.""" +
                 f""""\n{request.build_absolute_uri(reverse("approve_scavenger_puzzles"))}""")
             ScavConsumer.notify_trigger(photo.photo.url, team.display_name, photo.id)
