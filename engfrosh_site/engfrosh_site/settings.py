9ebeabeea24b49d8d3e3f0f3e5453d8d0d9054d1
finlaywashere <finman292004@protonmail.com>
2024-12-28 14:55:54 -0500

    Fixed garbage migrations and added testing mode that has sqlite db

--- engfrosh_site/engfrosh_site/settings.py
+++ engfrosh_site/engfrosh_site/settings.py
@@ -201,6 +201,10 @@ DATABASES = {
         }
     }}
 
+if 'test' in sys.argv:
+    print("!!TESTING MODE!!")
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}
+
 # Password validation & Authentication
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
---
+++ engfrosh_site/test.py
@@ -0,0 +1,1 @@
+import common_models.test_scav  # noqa: F401
