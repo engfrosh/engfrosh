9abbbcf623d565552532d9d240392210f024a19d
finlaywashere <finman292004@protonmail.com>
2025-02-25 13:10:34 -0500

    Fixed ICS api not using slugs

--- engfrosh_site/api/urls.py
+++ engfrosh_site/api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('photo', views.VerificationPhotoAPI.as_view(), name="photo"),
     path('calendar', views.CalendarAPI.as_view(), name="calendar_api"),
-    path('ics/<int:uid>', views.ICSAPI.as_view(), name="ics_api")
+    path('ics/<slug:uid>', views.ICSAPI.as_view(), name="ics_api")
 ]
